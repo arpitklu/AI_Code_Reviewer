db329e32d657b9c4b955e8d3ecd7a62e45675b66
arpitklu <arrpiit2004@gmail.com>
2025-07-28 23:53:38 +0530

    feat: AI-Code-Reviewer Project COMPLETED.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,8 @@ import "prismjs/themes/prism-tomorrow.css"
 import Editor from "react-simple-code-editor"
 import prism from "prismjs"
 import Markdown from "react-markdown"
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
 import axios from 'axios'
 import "./App.css";
 
@@ -53,7 +55,9 @@ const [review, setReview] = useState(``)
           className="review">Review</div>
         </div>
         <div className="right">
-          <Markdown>{review}</Markdown>
+          <Markdown
+            rehypePlugins={[rehypeHighlight]}
+          >{review}</Markdown>
         </div>
       </main>
     </>
